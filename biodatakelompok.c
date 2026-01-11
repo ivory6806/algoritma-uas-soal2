a5f9fc9a7d16baf7342cbd02a7ce3d62956f5263
ivory6806 <ivoryalvadiaskurniawan@gmail.com>
2026-01-12 06:17:50 +0700

    12/11/2026 - revisi anggota kelompok

--- biodatakelompok.c
+++ biodatakelompok.c
@@ -3,6 +3,5 @@ void biodatakelompok(){
     printf("251110018 - Ari Andrean \n");
     printf("251110014 - Ivory Alva Dias Kurniawan \n");
     printf("251110010 - Adhitiya Agung Kurniawan \n");
-    printf("251110072 - Rizal Zaelani \n");
-    printf("251110065 - Wira Suteratama \n \n");
+    printf("251110072 - Rizal Zaelani \n \n");
 }
